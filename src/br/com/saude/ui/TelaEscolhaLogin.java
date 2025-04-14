f578c3066b7fcbf5afba138f3a89aed02fc64be1
Victor Sousa de Carvalho <60471184+Vaanbaske@users.noreply.github.com>
2025-04-14 18:32:15 -0300

    Corrigido de novo v0.01

--- src/br/com/saude/ui/TelaEscolhaLogin.java
+++ src/br/com/saude/ui/TelaEscolhaLogin.java
@@ -16,24 +16,24 @@ public class TelaEscolhaLogin extends JFrame {
         titulo.setFont(new Font("Arial", Font.BOLD, 16));
         add(titulo, BorderLayout.NORTH);
 
-        JPanel painelBotoes = new JPanel(new GridLayout(3, 1, 10, 10)); // Agora tem 3 linhas!
+        JPanel painelBotoes = new JPanel(new GridLayout(3, 1, 10, 10)); 
         painelBotoes.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
 
         JButton botaoMedico = new JButton("Login Médico");
-        botaoMedico.setBackground(Color.decode("#ADD8E6")); // Azul claro
+        botaoMedico.setBackground(Color.decode("#ADD8E6")); // Azul claro - tem que ver se querem mudar as cores
         botaoMedico.addActionListener(e -> new TelaLogin());
 
         JButton botaoPaciente = new JButton("Login Paciente");
-        botaoPaciente.setBackground(Color.decode("#FFCCCC")); // Rosa claro
+        botaoPaciente.setBackground(Color.decode("#FFCCCC")); // Rosa claro - não gostei, mas não pensei em outra
         botaoPaciente.addActionListener(e -> new TelaLogin());
 
         JButton botaoCadastro = new JButton("Cadastro");
-        botaoCadastro.setBackground(Color.decode("#D3FAD3")); // Verde claro
+        botaoCadastro.setBackground(Color.decode("#D3FAD3")); // Verde claro - erro na cor corrigido
         botaoCadastro.addActionListener(e -> new TelaCadastroUsuario());
 
         painelBotoes.add(botaoMedico);
         painelBotoes.add(botaoPaciente);
-        painelBotoes.add(botaoCadastro); // 👈 Aqui adiciona o botão de cadastro
+        painelBotoes.add(botaoCadastro); //  Aqui adiciona o botão de cadastro - corrigido tava sem o botão 
 
         add(painelBotoes, BorderLayout.CENTER);
 
